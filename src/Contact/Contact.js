ace5caacad4afe0a33f3753cacdab7c3bfbda219
Vishal kumar <vishalk80269@gmail.com>
2022-06-15 21:09:10 +0530

    try to host on netlify

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -44,7 +44,7 @@ const home =<svg xmlns="http://www.w3.org/2000/svg"
         </h1>
     </Row>
     <Row className='text-center mt-5'>
-    {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7212.617504200288!2d84.80280099624407!3d25.32741850893124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398d4fba2c344831%3A0xaa772e27fa8f6371!2sMuskan%20Genral%20Store%20Panitanki%20Roaf!5e0!3m2!1sen!2sin!4v1655116606799!5m2!1sen!2sin"
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7212.617504200288!2d84.80280099624407!3d25.32741850893124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398d4fba2c344831%3A0xaa772e27fa8f6371!2sMuskan%20Genral%20Store%20Panitanki%20Roaf!5e0!3m2!1sen!2sin!4v1655116606799!5m2!1sen!2sin"
      width="600" 
      height="450"
       style={{border:0}} 
@@ -52,7 +52,7 @@ const home =<svg xmlns="http://www.w3.org/2000/svg"
       loading="lazy" 
       referrerpolicy="no-referrer-when-downgrade">
 
-      </iframe> */}
+      </iframe>
     </Row>
 </Container>
 {/* contact form */}
